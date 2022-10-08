78356069525fb0649710bb8bfd82e7e530c5e99d
Cancu <juancruz.cancu@gmail.com>
2022-10-08 15:45:51 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h2 className="text-orange-600">Hello world</h2>
+    </div>
+  );
+}
