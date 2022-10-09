849a9d5b988785e9816333feba89dd434b5b7029
Cancu <juancruz.cancu@gmail.com>
2022-10-08 21:52:35 -0300

    aa1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { useWeb3Auth } from "../services/web3Auth";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
-  const { provider } = useWeb3Auth();
-
-  console.log(provider);
-
   return (
     <div className="w-screen h-screen bg-[#FBE7E7] griddie flex justify-center items-center">
       <div className="col-start-3 col-end-7 flex flex-col gap-7">
@@ -20,9 +16,7 @@ export default function Home() {
         </span>
       </div>
       <div className="col-start-8 col-end-11">
-        <button onClick={login} className="card">
-          Login
-        </button>
+        <ConnectButton />
       </div>
     </div>
   );
