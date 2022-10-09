a2987e70cb6dd86f9725a2a3fb8df41ab9077b43
Cancu <juancruz.cancu@gmail.com>
2022-10-09 00:43:10 -0300

    only numbers

--- components/AddressPercentage.jsx
+++ components/AddressPercentage.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { FormControl, Input, InputAdornment } from "@mui/material";
 
 const AddressPercentage = () => {
   return (
-    <div className="flex gap-8 mb-2">
+    <div className="flex gap-4 mb-2">
       <input
         type="text"
-        className="bg-white rounded-xl p-2 text-black outline-0	flex-1"
+        className="bg-white rounded-xl p-2 text-black outline-0 flex-1"
         placeholder="Address or ENS"
       />
-      <input
-        type="text"
-        className="bg-white rounded-xl p-2 text-black outline-0	flex-4"
-        placeholder="%"
-      />
+      <FormControl sx={{ background: "white", borderRadius: 1 }}>
+        <Input
+          onChange={(e) => console.log(e)}
+          startAdornment={<InputAdornment position="start">%</InputAdornment>}
+          disableUnderline
+          type="number"
+        />
+      </FormControl>
     </div>
   );
 };
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../components/Layout";
 import { Input, TextField } from "@mui/material";
 import AddressPercentage from "../components/AddressPercentage";
 
-const dashboard = () => {
+const Dashboard = () => {
+  const [data, setData] = useState({
+    yearsAmount: 0,
+    inactiveTime: 0,
+    recipients: [],
+    amount: 0,
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <Layout>
       <div className="col-start-5 col-end-9 row-start-3 flex flex-col gap-3">
@@ -11,17 +22,29 @@ const dashboard = () => {
           How much time do you want to keep your funds locked?
         </label>
         <input
-          type="text"
+          type="number"
           className="bg-white rounded-xl p-2 text-black outline-0	"
           placeholder="Amount of Years"
+          onChange={(e) =>
+            setData({
+              ...data,
+              yearsAmount: Number(e.target.value),
+            })
+          }
         />
         <label className="text-black">
           After how much time should we consider your account inactive?
         </label>
         <input
-          type="text"
+          type="number"
           className="bg-white rounded-xl p-2 text-black outline-0	"
           placeholder="Amount in months"
+          onChange={(e) =>
+            setData({
+              ...data,
+              inactiveTime: Number(e.target.value),
+            })
+          }
         />
         <label className="text-black">
           Which address should receive what porcentage of your funds?
@@ -33,9 +56,15 @@ const dashboard = () => {
           Start your fund by suscribing your first Ether
         </label>
         <input
-          type="text"
+          type="number"
           className="bg-white rounded-xl p-2 text-black outline-0	"
           placeholder="Amount"
+          onChange={(e) =>
+            setData({
+              ...data,
+              amount: Number(e.target.value),
+            })
+          }
         />
         <button className="bg-[#FEAEA5] rounded-lg shadow-lg px-4 py-2 text-black text-xl mt-8">
           Next
@@ -45,4 +74,4 @@ const dashboard = () => {
   );
 };
 
-export default dashboard;
+export default Dashboard;
